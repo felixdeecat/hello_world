9bb6c5631fc4d43536a2e33d164191e60a265793
felixdeecat <felix_dee_cat@yahoo.com>
2016-06-14 13:58:41 -0500

    This is the demo python file

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+
+# a simple method to get input of a name and print
+def hello():
+    name = input("What is your name?")
+    print("Nice to meet you, "+name+", hope things are working for you!")
+
+# call the method
+hello()
