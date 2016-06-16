da6f1bb0086dd578736af932492f0975915c80ee
felixdeecat <felix_dee_cat@yahoo.com>
2016-06-16 14:02:23 -0500

    Added command line arguments to get file input
    Still some bugs / design to work on

--- GraphsDesign.py
+++ GraphsDesign.py
@@ -2,16 +2,22 @@
 
 import matplotlib.pyplot as plt
 import networkx as nx
+import sys
 
-G = nx.read_edgelist("C:\Users\TJ\GitHub\\brunson_revolution\out.brunson_revolution_revolution",comments="%")
+print sys.argv[1]
+
+#G = nx.read_edgelist("C:\Users\Thomas\Downloads\\brunson_revolution\out.brunson_revolution_revolution",comments="%")
+G = nx.read_edgelist(sys.argv[1],comments="%")
 H = nx.DiGraph(G)
-H.edges()
+print H.edges()
+
 
 num_nodes = G.number_of_nodes()
 print num_nodes
 
 #pos = nx.spring_layout(G, iterations = 10)
 
+nx.draw(H)
 nx.draw(G)
 
 #==============================================================================
